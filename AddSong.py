a07e24d74abdf00f7555974ee7b52f1f96ff674f
Ame <ame@kirisamekano.com>
2024-07-09 15:32:34 +0800

    little update

--- AddSong.py
+++ AddSong.py
@@ -8,60 +8,62 @@ import sys
 import time
 import platform
 
-arg = []
+def get_song_list(play_list_id):
+    url = "https://music.163.com/playlist?id=" + play_list_id
 
-argv_name = ['playlistId', 'addr', 'port', 'listId', 'token']
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+        'Cookie': 'os=pc'
+    }
 
-if len(sys.argv) == 6:
-    for i in range(5):
-        arg.append(sys.argv[i + 1])
-else:
-    print("Please enter the right arguments.")
-    exit()
+    response = requests.get(url, headers)
+    response.encoding = response.apparent_encoding
 
-url = "https://music.163.com/playlist?id=" + arg[0]
+    soup = BeautifulSoup(response.text, 'lxml')
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
-    'Cookie': 'os=pc'
-}
+    ul = soup.find('ul', class_ = 'f-hide')
+    a_list = ul.find_all('a')
 
-response = requests.get(url, headers = headers)
-response.encoding = response.apparent_encoding
+    data = {}
 
-soup = BeautifulSoup(response.text, 'lxml')
+    for a in a_list:
+        name_match = re.search(r'<a\s.*?>(.*?)<\/a>', str(a))
+        id_match = re.search(r'<a\s.*?href="/song\?id=(\d+)".*?', str(a))
+        data[id_match.group(1)] = name_match.group(1)
 
-ul = soup.find('ul', class_ = 'f-hide')
-a_list = ul.find_all('a')
+    data = json.dumps(data, indent = 2)
+    with open('song.json', 'w', encoding = 'utf-8') as file_json:
+        file_json.write(data)
 
-data = {}
+    if platform.system() == "Linux":
+        os.remove('song.json')
+    elif platform.system() == "Windows":
+        os.unlink('song.json')
 
-for a in a_list:
-    name_match = re.search(r'<a\s.*?>(.*?)<\/a>', str(a))
-    id_match = re.search(r'<a\s.*?href="/song\?id=(\d+)".*?', str(a))
-    data[id_match.group(1)] = name_match.group(1)
+def create_list(addr, port, list_id, token):
+    url = "http://" + addr + ":" + "/api/bot/use/0/(/list/create/id/id)"
+    surl = "http://" + addr + ":" + "/api/bot/use/0/(/list/list/id)"
 
-data = json.dumps(data, indent = 2)
-with open('song.json', 'w', encoding = 'utf-8') as file_json:
-    file_json.write(data)
+    headers = {
+        'Authorization': 'Basic ' + token,
+        'Content-Type': 'application/json'
+    }
 
-scriptPath = 'get.js'
+    response = requests.get(surl, headers)
+    print(response.text)
 
-def subprocess_script():
-    process = subprocess.Popen(['node', scriptPath, arg[1], arg[2], arg[3], arg[4]], stdout=subprocess.PIPE, text=True, encoding = 'utf-8')
-    stdout, stderr = process.communicate()
+def main():
+    if len(sys.argv) == 6:
+        play_list_id = sys.argv[1]
+        addr = sys.argv[2]
+        port = sys.argv[3]
+        list_id = sys.argv[4]
+        token = sys.argv[5]
+    else:
+        print("Please enter the right arguments.")
+        exit()
 
-    print(stdout)
-    print(stderr)
-    process.wait()
-    print(process.returncode)
+    get_song_list(play_list_id)
+    create_list(addr, port, list_id, token)
 
-subprocess_script()
-time.sleep(10)
-
-file_json.close()
-
-if platform.system() == "Linux":
-    os.remove('song.json')
-elif platform.system() == "Windows":
-    os.unlink('song.json')
+main()
