c93ecf234a8d41b098a5d6a0ee47751b704ae376
Karol <ame@kirisamekano.com>
2024-07-24 18:00:35 +0800

    count update

--- AddSong.py
+++ AddSong.py
@@ -2,23 +2,30 @@ import requests
 import json
 import sys
 import urllib.parse
+import time
+import math
 
 def get_song_list_kg(play_list_id, headers):
-    url = "http://121.37.225.70:4001/playlist/track/all?id=" + play_list_id
+    url_src = "http://121.37.225.70:4001/playlist/track/all?pagesize=300&id=" + play_list_id
+    response = requests.get(url_src, headers=headers)
+    count = json.loads(response.text)['data']['count']
+    for i in range(math.ceil(count / 300)):
+        i += 1
+        url = "http://121.37.225.70:4001/playlist/track/all?page=" + i + "&pagesize=300&id=" + play_list_id
 
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
-    data = data['data']['info']
-    song_list_len_src = len(data)
-    song_list_name = []
-    song_list_hash = []
-    for i in range(song_list_len_src):
-        song_name = data[i]['name']
-        song_hash = data[i]['hash']
-        song_list_name.append(song_name)
-        song_list_hash.append(song_hash)
+        response = requests.get(url, headers=headers)
+        data = json.loads(response.text)
+        data = data['data']['info']
+        song_list_len_src = len(data)
+        print(f"song list len = {song_list_len_src}")
+        song_list_name = []
+        song_list_hash = []
+        for i in range(song_list_len_src):
+            song_name = data[i]['name']
+            song_hash = data[i]['hash']
+            song_list_name.append(song_name)
+            song_list_hash.append(song_hash)
 
-    print(song_list_name)
     return song_list_name, song_list_hash, song_list_len_src
 
 def get_song_url_kg(song_list_name, song_list_hash, song_list_len):
@@ -31,8 +38,11 @@ def get_song_url_kg(song_list_name, song_list_hash, song_list_len):
         
         if "url" in data:
             url_list.append([[song_list_name[i]], data['url'][0]])
+            print(f"Import successfully: {song_list_name[i]}")
         else:
             print(f"Not import success: {song_list_name[i]}")
+
+        time.sleep(0.5)
 
     return url_list
 
@@ -63,13 +73,14 @@ def add_song_kg(addr, port, song_list, song_list_len, listId, song_begin_number,
         song_url = song_list[i][1]
         songurl_trans = urllib.parse.quote(song_url, safe='')
         addurl = "http://" + addr + ":" + port + "/api/bot/use/0/(/list/add/" + listId + "/" + songurl_trans + ")"
-        nameurl = "http://" + addr + ":" + port + "/api/bot/use/0/(/list/item/name/" + str(listId) + "/" + str(song_begin_number) + "/" + str(song_name)
+        nameurl = "http://" + addr + ":" + port + "/api/bot/use/0/(/list/item/name/" + listId + "/" + str(song_begin_number) + "/" + song_name
 
         response = requests.get(addurl, headers=headers)
         response1 = requests.get(nameurl, headers=headers)
 
         print(f"Add song {song_name} to the list")
-
+        song_begin_number += 1
+        time.sleep(1)
 
 def main():
     print("Script start...\n")
@@ -93,6 +104,6 @@ def main():
     song_list_name, song_list_hash, song_list_len = get_song_list_kg(play_list_id, headers_auth)
     url_list = get_song_url_kg(song_list_name, song_list_hash, song_list_len)
     list_len = len(url_list)
-    add_song_kg(addr, port, url_list, list_len, list_id, song_number, headers_auth)
+    # add_song_kg(addr, port, url_list, list_len, list_id, song_number, headers_auth)
 
 main()
